43e08ea43046f5c2b6c812652c773b30a6317757
Rishit Pathak <itsrishitpathak@gmail.com>
2026-01-15 09:47:10 +0530

    Add RetroFit for Currency Feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
-    
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     // --- NEW: ROOM DATABASE LIBRARIES ---
     val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
